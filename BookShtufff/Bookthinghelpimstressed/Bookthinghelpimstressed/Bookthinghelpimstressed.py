eb693f89dfd25a1040eb732804ad166a5df9b485
Chase <cagoo335@learner.dvusd.org>
2026-04-23 14:00:55 -0700

    ahhhhhhhhhhhhhhhhhhhhh

--- BookShtufff/Bookthinghelpimstressed/Bookthinghelpimstressed/Bookthinghelpimstressed.py
+++ BookShtufff/Bookthinghelpimstressed/Bookthinghelpimstressed/Bookthinghelpimstressed.py
@@ -1,18 +1,17 @@
 ﻿import requests
 from bs4 import BeautifulSoup
 
-url = 'http://books.toscrape.com/'
+url = "http://books.toscrape.com/"
 response = requests.get(url)
 
 if response.status_code == 200:
-    soup = BeautifulSoup(response.txt, 'html.parser')
+    soup = BeautifulSoup(response.text, "html.parser")
 
-    books = soup.find_all('article', class_='product_pods')
+    books = soup.find_all("article", class_="product_pod")
 
     for book in books:
-        title = book.h3.a['title']
-        price = book.find('p', class_='price_color').text
-
+        title = book.h3.a["title"]
+        price = book.find("p", class_="price_color").text
         print(title, price)
 else:
     print("Error:", response.status_code)
